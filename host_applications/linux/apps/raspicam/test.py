178de3e58bd250fb71baaa1124d31356e88001b9
Rocky Mussadi <rmussadi@cisco.com>
2019-09-18 01:57:33 +0000

    generate image from np array

--- host_applications/linux/apps/raspicam/test.py
+++ host_applications/linux/apps/raspicam/test.py
@@ -3,16 +3,21 @@
 import ctypes as ct
 import sys
 import numpy as np
+from PIL import Image
 
 def greater_than(a,b, data):
-    print(np.frombuffer(ct.cast(data, ct.POINTER(ct.c_uint8 * 10)).contents, dtype=np.uint8, count=10).reshape((10,1)))
-    #print np.frombuffer(data, ct.POINTER(ct.c_uint8 * 10).contents,dtype=np.uint8, count=10)
+    npa = np.frombuffer(ct.cast(data, ct.POINTER(ct.c_uint8 * 10)).contents, dtype=np.uint8, count=10).reshape((5,2))
     if a > b:
         print a,b, 'greater'
-        return 1
+        print npa *5
     else:
         print b,a, 'lesser'
-        return 0
+        print npa
+
+    npa[1,1] = 22
+    print npa
+    npa[10,1] = 44
+    return 1
 
 
 callback_type = ct.CFUNCTYPE(ct.c_int, ct.c_float, ct.c_float, ct.POINTER(ct.c_byte))
@@ -21,5 +26,15 @@ callback_func = callback_type(greater_than)
 _stilllib = ct.CDLL('libtq84.so')
 _stilllib.callmeback.argtype = (callback_type)  # Just so python doesn't have to guess the arg types
 #_stilllib.callmeback.argtypes = _stilllib.callmeback.argtype = (callback_type) # Just so python doesn't have to guess the arg types
-
 _stilllib.callmeback(callback_func)
+
+def create_pil_image():
+    # gradient between 0 and 1 for 256*256
+    array = np.linspace(0,1,256*256)
+
+    # reshape to 2d
+    mat = np.reshape(array,(256,256))
+
+    # Creates PIL image
+    img = Image.fromarray(np.uint8(mat * 255) , 'L')
+    img.save("joe.bmp")
